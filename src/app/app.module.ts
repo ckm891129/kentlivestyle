99b603dc8f0f4dbeb3c7740a8cebff686c6bd98d
kent-livestyle <ckm_kent@hotmail.com>
2022-05-05 15:38:08 +0800

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeightComponent } from './weight/weight.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeightComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
