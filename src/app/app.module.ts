7ba2aa5d2aad2faf031cfae501757ecc6490fc8d
kent-livestyle <ckm_kent@hotmail.com>
2022-05-05 22:17:06 +0800

    change of the source

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { WeightComponent } from './weight/weight.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    WeightComponent,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
