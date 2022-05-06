94e04dc46450df6546c0987002023b261c99c4fd
kent-livestyle <ckm_kent@hotmail.com>
2022-05-06 13:06:45 +0800

    Load page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
+
+// For Material Navigation
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
 import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module'
 import { AppComponent } from './app.component';
-import { WeightComponent } from './weight/weight.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar/sidebar.component';
-// import { ComponentsModule } from './components/components.module';
-
-
-// All replace to routingComponents
-// import { WeightCalcComponent } from './weight-calc/weight-calc.component';
-import { TestimonialComponent } from './testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
+    
     // TestimonialComponent,
     //  WeightCalcComponent
   ],
-  imports: [AppRoutingModule, BrowserModule],
+  imports: [
+    AppRoutingModule,
+     BrowserModule,
+     BrowserAnimationsModule,
+     MatToolbarModule,
+     MatSidenavModule,
+     MatButtonModule,
+     MatIconModule,
+     MatDividerModule
+    ],
   bootstrap: [AppComponent],
   providers: []
 })
